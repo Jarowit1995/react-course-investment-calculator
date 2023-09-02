f100b62918a9c2285b91c41244dc1f68a9039d96
Jaroslaw Mazurek <jaroslawmazurek1995@gmail.com>
2023-09-02 10:42:40 +0200

    finished project

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -24,14 +24,14 @@ const UserInput = (props) => {
     setUserInput((prevInput) => {
       return {
         ...prevInput,
-        [input]: value,
+        [input]: +value, // the "+" converts the string value to a number
       };
     });
   };
 
   return (
-    <form onSubmit={submitHandler} className='form'>
-      <div className='input-group'>
+    <form onSubmit={submitHandler} className={classes.form}>
+      <div className={classes['input-group']}>
         <p>
           <label htmlFor='current-savings'>Current Savings ($)</label>
           <input
@@ -55,7 +55,7 @@ const UserInput = (props) => {
           />
         </p>
       </div>
-      <div className='input-group'>
+      <div className={classes['input-group']}>
         <p>
           <label htmlFor='expected-return'>
             Expected Interest (%, per year)
@@ -81,11 +81,15 @@ const UserInput = (props) => {
           />
         </p>
       </div>
-      <p className='actions'>
-        <button onClick={resetHandler} type='reset' className='buttonAlt'>
+      <p className={classes.actions}>
+        <button
+          onClick={resetHandler}
+          type='reset'
+          className={classes.buttonAlt}
+        >
           Reset
         </button>
-        <button type='submit' className='button'>
+        <button type='submit' className={classes.button}>
           Calculate
         </button>
       </p>
